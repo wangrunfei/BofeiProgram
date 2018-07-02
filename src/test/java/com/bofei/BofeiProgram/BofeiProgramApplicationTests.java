18ade8a08e4a59caea045d5d94be1507d87b3664
wangrunfei <478074785@qq.com>
2018-07-02 14:33:40 +0800

    springboot壳子
    添加了web、Security、Devtools支持

---
+++ src/test/java/com/bofei/BofeiProgram/BofeiProgramApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bofei.BofeiProgram;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BofeiProgramApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
