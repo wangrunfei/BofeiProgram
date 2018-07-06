b7fd443a1adc1f1c98c48d606e3ecd6814150dbc
wb123852 <wb123852@126.com>
2018-07-06 22:17:07 +0800

    ...

--- src/main/java/com/bofei/BofeiProgram/frame/mapper/UserMapper.java
+++ src/main/java/com/bofei/BofeiProgram/frame/mapper/UserMapper.java
@@ -2,12 +2,14 @@ package com.bofei.BofeiProgram.frame.mapper;
 
 import com.bofei.BofeiProgram.frame.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface UserMapper {
 
+    @Select("select * from sys_user")
     public List<User> findUserInfo();
 
     public int addUserInfo(User user);
